e0b467709b87d15c994a5241e56b1655ecf75664
Sven van Putten <svenvanputten@gmail.com>
2020-11-27 14:07:21 +0100

    Moved the MainActivity and ColorFragment to a new 'ui' package

--- app/src/main/java/com/example/madlevel6task1/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel6task1/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.madlevel6task1
+package com.example.madlevel6task1.ui
 
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.madlevel6task1.R
 
 class MainActivity : AppCompatActivity() {
 
