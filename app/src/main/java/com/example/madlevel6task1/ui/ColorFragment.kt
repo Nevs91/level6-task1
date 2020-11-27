1e71e2631328aa8028e44cbbe77de46a05e6692d
Sven van Putten <svenvanputten@gmail.com>
2020-11-27 14:17:34 +0100

    Implemented the observe method for color items inside the viewModel

--- app/src/main/java/com/example/madlevel6task1/ui/ColorFragment.kt
+++ app/src/main/java/com/example/madlevel6task1/ui/ColorFragment.kt
@@ -1,15 +1,17 @@
 package com.example.madlevel6task1.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel6task1.R
 import com.example.madlevel6task1.adapter.ColorAdapter
 import com.example.madlevel6task1.model.ColorItem
+import com.example.madlevel6task1.viewModel.ColorViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_color.*
 
@@ -20,6 +22,8 @@ class ColorFragment : Fragment() {
 
     private val colors = arrayListOf<ColorItem>()
     private lateinit var colorAdapter: ColorAdapter
+
+    private val viewModel: ColorViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -36,10 +40,18 @@ class ColorFragment : Fragment() {
         rvColors.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         rvColors.adapter = colorAdapter
 
-        //observeColors()
+        observeColors()
     }
 
     private fun onColorClick(colorItem: ColorItem) {
         Snackbar.make(rvColors, "This color is: ${colorItem.name}", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun observeColors() {
+        viewModel.colorItems.observe(viewLifecycleOwner, {
+            colors.clear()
+            colors.addAll(it)
+            colorAdapter.notifyDataSetChanged()
+        })
     }
 }
